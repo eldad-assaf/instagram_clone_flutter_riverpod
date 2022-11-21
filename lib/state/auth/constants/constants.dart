604aaf48fe75858d94451978898a7391f74cd2db
eldad-assaf <eldadux@gmail.com>
2022-11-21 16:56:39 +0200

    sync

---
+++ lib/state/auth/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class Constants {
+  static const accountExsistsWithDifferentCredential =
+      'accont-exsists-with-different-credential';
+  static const googleCom = 'google.com';
+  static const googleScope = 'email';
+  const Constants._();
+}
