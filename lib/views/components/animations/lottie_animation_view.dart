97c9311f97d303ee373ec08559a238ecd756681c
eldad-assaf <eldadux@gmail.com>
2022-11-29 10:45:21 +0200

    sync

---
+++ lib/views/components/animations/lottie_animation_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_eldad/views/components/animations/models/lottie_animation.dart';
+import 'package:lottie/lottie.dart';
+
+class LottieAnimationView extends StatelessWidget { 
+  final LottieAnimation animation;
+  final bool repeat;
+  final bool reverse;
+  const LottieAnimationView({
+    super.key,
+    required this.animation,
+    this.repeat = true,
+    this.reverse = false,
+  });
+
+  @override
+  Widget build(BuildContext context) =>
+      Lottie.asset(animation.fullPath, reverse: reverse, repeat: repeat);
+}
+
+extension GetFullPath on LottieAnimation {
+  String get fullPath => '/assets/animations/$name.json';
+}
