3ce57d5ac108ad1c85452afe8e16c5907f3dbe8c
eldad-assaf <eldadux@gmail.com>
2022-11-30 15:21:58 +0200

    fix

--- lib/views/components/animations/lottie_animation_view.dart
+++ lib/views/components/animations/lottie_animation_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone_eldad/views/components/animations/models/lottie_animation.dart';
 import 'package:lottie/lottie.dart';
 
-class LottieAnimationView extends StatelessWidget { 
+class LottieAnimationView extends StatelessWidget {
   final LottieAnimation animation;
   final bool repeat;
   final bool reverse;
@@ -19,5 +19,5 @@ class LottieAnimationView extends StatelessWidget {
 }
 
 extension GetFullPath on LottieAnimation {
-  String get fullPath => '/assets/animations/$name.json';
+  String get fullPath => 'assets/animations/$name.json';
 }
