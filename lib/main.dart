c03619ff9ad0b476330bf36107863f7748682e4d
eldad-assaf <eldadux@gmail.com>
2022-12-07 13:26:26 +0200

    sync

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:instagram_clone_eldad/state/auth/providers/auth_state_provider.dart';
 import 'package:instagram_clone_eldad/state/providers/is_loading_provider.dart';
 import 'package:instagram_clone_eldad/state/auth/providers/is_logged_in_provider.dart';
 import 'package:instagram_clone_eldad/views/components/loading/loading_screen.dart';
