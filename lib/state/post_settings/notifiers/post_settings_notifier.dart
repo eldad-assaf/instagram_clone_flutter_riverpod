16a1dd027630473bef086e129b9912b14f33c106
eldad-assaf <eldadux@gmail.com>
2022-12-08 16:15:37 +0200

    sync

--- lib/state/post_settings/notifiers/post_settings_notifier.dart
+++ lib/state/post_settings/notifiers/post_settings_notifier.dart
@@ -1,6 +1,23 @@
+import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone_eldad/state/post_settings/models/post_settings.dart';
 
-class PostSettingNotifier extends StateNotifier<> {
+class PostSettingNotifier extends StateNotifier<Map<PostSetting, bool>> {
+  PostSettingNotifier()
+      : super(UnmodifiableMapView({
+          for (final setting in PostSetting.values) setting: true,
+        }));
 
+  void setSettings(
+    PostSetting setting,
+    bool value,
+  ) {
+    // get the existing value
+    final exsistingValue = state[setting];
+    if (exsistingValue == null || exsistingValue == value) {
+      return;
+    }
 
+    state = Map.unmodifiable(Map.from(state)..[setting] = value);
+  }
 }
